01e6747fc5af3240481d3f2c4715b0e8947390de
Nicolas Bloj <nicolasbloj@gmail.com>
2017-11-23 19:06:12 -0300

    firebase app config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBF2lVqSOvmgJBJbAThNayDTkHhngKTBO4',
+    authDomain: 'mayco-backend.firebaseapp.com',
+    databaseURL: 'https://mayco-backend.firebaseio.com',
+    projectId: 'mayco-backend',
+    storageBucket: '',
+    messagingSenderId: '211952405898'
+  }
 };
