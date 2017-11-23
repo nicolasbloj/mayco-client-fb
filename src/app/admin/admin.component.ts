ad1b4dbf0d953b57b3f43ab7d48fe322c0bac9b7
Nicolas Bloj <nicolasbloj@gmail.com>
2017-11-23 17:49:26 -0300

    admin

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nr-admin',
+  templateUrl: './admin.component.html',
+  styles: []
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
