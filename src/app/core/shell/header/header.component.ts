ca5cbeba50c8124b6135a7037278f11e7788b283
Nicolas Bloj <nicolasbloj@gmail.com>
2017-11-24 11:10:31 -0300

    nav

--- src/app/core/shell/header/header.component.ts
+++ src/app/core/shell/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  PATH_LOGO = 'assets/logo_sin_fondo.png';
+
   constructor() { }
 
   ngOnInit() {
